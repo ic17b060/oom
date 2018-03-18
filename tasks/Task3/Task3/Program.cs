4ed5dce6a21c24e6ca8beab9b823a9c5b7164117
ic17b060 <ic17b060@technikum-wien.at>
2018-03-18 23:35:28 +0100

    add project Task3

--- tasks/Task3/Task3/Program.cs
+++ tasks/Task3/Task3/Program.cs
@@ -19,44 +19,49 @@ namespace Task3
 
     public interface IGame
     {
-        string Description { get; }
-        decimal GetPrice(Currency currency);
+        string Title { get; }
+        string Console { get; }
+        int Year { get; }
+        Currency Currency { get; }
+
+
+        decimal GetPrice();
     }
 
     public class DLC : IGame
     {
-        public decimal amount;
 
         public bool IsDownloaded { get; private set; }
 
-        public DLC(decimal amount, Currency currency)
-        {
-            if (amount <= 0) throw new ArgumentException("Der Betrag muss größer als 0 sein!", nameof(amount));
+        public string Description { get; }
+        public string Title { get; }
+        public string Console { get; }
+        public int Year { get; }
+        public string Code { get; }
+        public Currency Currency { get; }
 
-            Amount = amount;
+        private decimal price;
+
+        public DLC(string title, string console, int year, decimal price, Currency currency)
+        {
+            if (price <= 0) throw new ArgumentException("Der Betrag muss größer als 0 sein!", nameof(price));
+            Title = title;
+            Console = console;
+            Year = year;
+            this.price = price;
             Currency = currency;
             Code = Guid.NewGuid().ToString();
             IsDownloaded = false;
         }
-
-        public decimal Amount { get; }
-
-        public string Description { get; }
-
-        public Currency currency { get; }
-
-        public string Code { get; }
 
         public void Download()
         {
             if (IsDownloaded) throw new InvalidOperationException($"DLC {Code} has already been downloaded!");
         }
 
-        public decimal GetPrice(Currency currency)
+        public decimal GetPrice()
         {
-            if (this.currency == currency) return Amount;
-            else throw new ArgumentException("Falsche Währung!", nameof(currency)); 
-
+            return price;
         }
     }
 
@@ -67,19 +72,28 @@ namespace Task3
         private int year;
         private decimal price;
         private Currency currency;
-        private decimal Amount;
+        private decimal amount;
 
-        public string GetTitle() { return title; }
-        public string GetConsole() { return console; }
-        public int GetYear() { return year; }
-        public decimal GetPrice() { return price; }
+        public string Description { get; }
+        public string Title { get; }
+        public string Console { get; }
+        public int Year { get; }
         public string Code { get; }
+        public Currency Currency { get; }
 
-        public void SetYear(int newYear)
+        public VideoGame(string title, string console, int year, decimal price, Currency currency)
         {
-            if (newYear < 1992 || newYear > 2013) throw new Exception("Undefiniertes Jahr!");
-            year = newYear;
+            if (title == null || title == "") throw new Exception("Leerer Titel!");
+            if (console == null || console == "") throw new Exception("Keine Konsole definiert!");
+            if (price < 0) throw new Exception("Negativer Preis!");
+            if (year < 1992 || year > 2017) throw new Exception("Undefiniertes Jahr!");
+            Title = title;
+            Console = console;
+            Year = year;
+            SetPrice(price);
+            this.currency = currency;
         }
+
 
         public void SetPrice(decimal newPrice)
         {
@@ -87,52 +101,39 @@ namespace Task3
             price = newPrice;
         }
 
-        public decimal GetPrice(Currency currency)
+        public decimal GetPrice()
         {
-            
-            if (this.currency == currency) return Amount;
-            else throw new ArgumentException("Falsche Währung!", nameof(currency)); 
+            return price;
         }
 
-        public string Description => "DLC" + Code;
 
-        public VideoGame(string a, string b, int y, decimal p, decimal c)
+    }
+    class Program
+    {
+        static void Main(string[] args)
         {
-            if (a == null || a == "") throw new Exception("Leerer Titel!");
-            if (b == null || b == "") throw new Exception("Keine Konsole definiert!");
-            if (y < 1992 || y > 2013) throw new Exception("Undefiniertes Jahr!");
-            if (p < 0) throw new Exception("Negativer Preis!");
-            SetYear(y);
-            title = a;
-            console = b;
-            year = y;
-            price = p;
-            currency = c;
-        }
-
-        class Program
-        {
-            static void Main(string[] args)
+            try
             {
-                try
+                var games = new IGame[]
                 {
-                    VideoGame a = new VideoGame("The Legend of Zelda: A Link to the Past,", "Super Nintendo,", 1992, 25.13, Currency.EUR);
-                    Console.WriteLine($"a. {a.GetTitle()} {a.GetConsole()} {a.GetYear()} {a.GetPrice()}");
-                    VideoGame b = new VideoGame("Donkey Kong Country,", "Super Nintendo,", 1994, 28.15, Currency.EUR);
-                    Console.WriteLine($"b. {b.GetTitle()} {b.GetConsole()} {b.GetYear()} {b.GetPrice()}");
-                    VideoGame c = new VideoGame("Final Fantasy 8,", "Playstation,", 1999, 2800, Currency.YEN);
-                    Console.WriteLine($"c. {c.GetTitle()} {c.GetConsole()} {c.GetYear()} {c.GetPrice()}");
-                    VideoGame d = new VideoGame("The Last of Us,", "Playstation 4,", 2013, 75.68, Currency.USD);
-                    Console.WriteLine($"d. {d.GetTitle()} {d.GetConsole()} {d.GetYear()} {d.GetPrice()}");
+                    new VideoGame("The Legend of Zelda: A Link to the Past", "Super Nintendo", 1992, 25.13m, Currency.EUR),
+                    new VideoGame("Donkey Kong Country", "Super Nintendo", 1994, 28.15m, Currency.EUR),
+                    new VideoGame("Final Fantasy 8", "Playstation", 1999, 2800m, Currency.YEN),
+                    new VideoGame("The Last of Us", "Playstation 4", 2013, 75.68m, Currency.USD),
+                    new DLC("Final Fantasy XV DLC 4", "Playstation 4", 2017, 7.90m, Currency.EUR)
+                };
+
+                foreach (var x in games)
+                {
+                    Console.WriteLine($"{x.Title,-40} {x.Console,-20} {x.Year,-8} {x.GetPrice()} {x.Currency,-5}");
                 }
 
-                catch(IndexOutOfRangeException)
-                {
-                    Console.WriteLine("CATCH");
-                }
-                Console.WriteLine("DONE");
             }
-
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("CATCH");
+            }
         }
+
     }
 }
